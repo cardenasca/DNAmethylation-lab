249bdf9bad3fb6f2874614c9d682f0b49afffe50
hhhh5 <hhhh5@posteo.de>
2020-05-28 22:14:42 -0500

    Rewrite continued

--- code/meth01_process_data.R
+++ code/meth01_process_data.R
@@ -107,7 +107,7 @@ meth %>% control_metrics %>% sample_failure -> pheno$failed
 # The line above is equivalent to (i.e., could be alternatively written as)
 # pheno$failed = sample_failure(control_metrics(meth))
 
-# QUESTION: Are there any failed samples?
+# QUESTION: How many samples failed this check?
 
 
 # ------------------------
@@ -130,8 +130,8 @@ detP = meth$detP[chrY,]
 # Count those below 0.01 as detected
 detP = colSums(detP<0.01,na.rm=TRUE)
 
-boxplot(split(detP,pheno$predicted_sex),ylab="# of detected Y chromosome probes")
-split(detP,pheno$predicted_sex) %>% sapply(mean)
+boxplot(split(detP,pheno$sex),ylab="# of detected Y chromosome probes")
+split(detP,pheno$sex) %>% sapply(median)
 
 # Almost all of the 416 chromosome probes are called detected in the males whereas
 # among females 60 probes are detected on average.
@@ -150,7 +150,7 @@ split(detP,pheno$predicted_sex) %>% sapply(mean)
 # intensities in the red color channel, the so-called dye bias. For probes of
 # Infinium type II design, which use separate color channels to measure the
 # methylated and unmethylated signal, this results in a shifted distribution of
-# beta-values. (Probes of Infinium design type I are not affected, as they measure
+# betavalues. (Probes of Infinium design type I are not affected, as they measure
 # both signals in the same color channel.)
 
 # `correct_dye_bias()` adjusts the green color channel using the red color channel
@@ -160,17 +160,17 @@ meth %>% dont_normalize                      -> with_bias
 meth %>% correct_dye_bias %>% dont_normalize -> corrected
 
 # Looking at the same loci as before we can check whether dye-bias correction
-# changed the beta-values
+# changed the beta values
 # Each probe is of a different type/color channel
 meth$manifest$channel[201:203]
 
 with_bias[201:203,1:3] %>% round(4)
 corrected[201:203,1:3] %>% round(4)
 
-# QUESTION: Why are the beta-values for cg06091566 unchanged?
+# QUESTION: Why are the beta values for cg06091566 unchanged?
 
 
-# Plotting beta-values for heterozygous SNPs, we can observe the dye bias as a
+# Plotting beta values for heterozygous SNPs, we can observe the dye bias as a
 # deviation from 0.5. For the corrected data, the middle peak aligns with 0.5
 snps = meth$manifest[probe_type=="rs" & channel=="Both"]$index
 
@@ -219,7 +219,7 @@ text(Y~X,labels=sex,col=ifelse(sex=="m",2,1),data=pheno)
 # cut-off of 0.01
 meth = mask(meth,0.01)
 
-# Now we can compute a "clean" matrix of beta-values. Undetected probes are
+# Now we can compute a "clean" matrix of beta values. Undetected probes are
 # set to NA
 beta = dont_normalize(meth) 
 
@@ -238,89 +238,118 @@ stripchart(pheno$outlier,method="jitter",pch=4)
 abline(v=-4,lty="dotted",col=2)
 
 # QUESTION: What is the ID of the sample failing this check? Compare the answer
-# to the answer of question _.
+# to the answer of the previous question. What can we infer from this?
 
 
 # Mark the failing sample for exclusion
-pheno[outlier>-4,exclude:=TRUE]
+pheno[outlier > -4,exclude:=TRUE]
 
 
 
-
+# ------------------------
+# Check for duplicates
+# The function `enumerate_sample_donors()` can be used to find samples with the 
+# same genetic fingerprint
 pheno$donor_id = enumerate_sample_donors(genotypes)
+
+# Find donor IDs represented more than once
 pheno[,n:=.N,by=donor_id]
 pheno[n>1,.(gsm,donor_id)]
 
 pheno[gsm=="GSM2260543",exclude:=TRUE] # drop duplicate
 
-#' ## Principal component analysis
-#' Principal component analysis is a popular feature reduction method: it projects high-dimensional data into a lower-dimensional representation while trying to retain as much variability as possible. This is especially useful when either individual features are highly correlated and it is therefore reasonable to summarize them, or when (sometimes subtle) traces of background effects can be found across of large number of features.
-#' 1. Get of subset of beta without probes on the X and Y chromosome
+# QUESTION: How many different donors are there?
 
-chrXY = meth$manifest$chr %in% c("X","Y") | meth$manifest$probe_type == "rs"
+# ------------------------
+# Principal component analysis
+# PCA is a popular feature reduction method: it projects high-dimensional data
+# onto a lower-dimensional representation while trying to retain as much
+# variability as possible.
+
+set.seed(292846330)
+
+# We will apply PCA to the matrix of beta but without the probes targeting the
+# allosomes. Exclude SNP probes as well.
+chrXY =  meth$manifest[ chr %in% c("X","Y") | probe_type == "rs"]$index
 pcs = beta[-chrXY,]
 pcs = pcs - rowMeans(pcs)
 pcs = na.omit(pcs)
 pcs = t(pcs)
-pcs = trlan.svd(pcs,neig=2)
+pcs = trlan.svd(pcs,neig=2) # Just the first two principal components
 pcs = pcs$u
 
 pheno$pc1 = pcs[,1]
 pheno$pc2 = pcs[,2]
 
 plot(pc2~pc1,col=ifelse(sex=="m",2,1),data=pheno)
-text(pc2~pc1,labels=pheno[34]$gsm,data=pheno[34],pos=4,offset=1,col=2)
+text(pc2~pc1,labels=pheno[34]$gsm,data=pheno[34],pos=2,offset=1,col=2)
+
+# GSM2219539 is actually a lung tissue sample from another GEO dataset. It
+# dominates the first principal component and should be excluded as it could
+# drastically change the results of the downstream analysis.
 
 pheno[gsm=="GSM2219539",exclude:=TRUE]
 
-#' GSM2219539 is actually a lung tissue sample from another GEO dataset. It dominates the first principal component and should be excluded as it otherwise could drastically change the results of downstream analyses.
-#' PCA may be applied iteratively. After excluding samples that manifest as outliers, repeating PCA can give very different principal components.
-#'
+# PCA may be applied iteratively. After excluding samples that manifest as
+# outliers, repeating PCA can give very different principal components.
 
-#' ## Leukocyte composition
-#' This quality check will only apply in case of blood samples (blood is, however, one of the most commonly studied tissues). The function `estimateLC()` implements the [Houseman method](https://doi.org/10.1186/1471-2105-13-86) to predict the leukocyte composition. The user has the choice between various sets of model parameters trained on various reference datasets (see `?estimateLC` for a list of options). The function operates on the matrix of beta-values.
-#' 
-#' 1. Estimate the leukocyte composition using the function `estimateLC`. What does the function return?
-#' 2. Add the cell proportion estimates as new columns to the `pheno` data.table using the function `cbind`
-#' 3. Plot the proportions of granulocytes versus (the column named `GR`) using
-#' 4. Set the `exclude` flag TRUE for the conspicuous sample
 
+
+# ------------------------
+# Leukocyte composition
+# This quality check will only apply in case of blood samples (blood is, however,
+# one of the most commonly studied tissues). The function `estimateLC()`
+# implements the Houseman method (doi.org/10.1186/1471-2105-13-86) to predict the
+# leukocyte composition. The user has the choice between various sets of model
+# parameters trained on various reference datasets (see `?estimateLC` for a list
+# of options). The function operates on the matrix of beta values.
+
+
+# Here we are using the Reinius reference dataset
 LC = estimateLC(beta,ref="Reinius")
 
-stripchart(rowSums(LC),xlab="Sum of cell proportions",m="jitter")
-abline(v=1,lty=3,col=2)
-
+# `LC` is a data.table. We can easily add to `pheno`
 pheno = cbind(pheno,LC)
 
-plot(pheno$GR,ylim=c(0,1))
+# Plot the proportions of granulocytes versus (the column named `GR`)
+plot(sort(pheno$GR),ylim=c(0,1))
+
+# We observe two outliers, a sample with a very low proportion of granulocytes,
+# and one with a proportion close to 100%
 
 pheno[which.min(GR),.(gsm,exclude)]
 #' This is the lung tissue sample from before
 
 pheno[which.max(GR),.(gsm,exclude)]
-#' This is actually a sample of purified granulocytes
-
+#' This is actually a sample of purified granulocytes. Flag for exclusion.
 pheno[gsm=="GSM1185585",exclude:=TRUE]
 
 
-#' LC stratified by smoking status
-#' 
+
+# Let's confirm that the leukocyte composition is a confounded with smoking status
+# LC stratified by smoking status
 LC$smoker = pheno$smoker
 LC = melt(LC,value.name="proportion",variable.name="cell_type",id.vars="smoker")
 
-boxplot(proportion ~ smoker+cell_type,LC,col=1:2,main="Cell type distribution by smoking status",xaxt="n")
+boxplot(proportion ~ smoker+cell_type,LC,col=1:2,
+	main="Cell type distribution by smoking status",xaxt="n")
 axis(1,at=seq(from=1.5, to=11.5,by=2),adj=1,labels=unique(LC$cell_type))
 legend("topright",c("Non-smoker","Smoker"),pch=15,bty='n',col=1:2)
 
-# drop problematic samples
+
+# ------------------------
+# Clean up
+
+# Keep only samples that passed all quality checks
 keep = which(!pheno$exclude)
-
-# drop columns no longer needed
-pheno = pheno[,.(gsm,smoker,sex,CD4,CD8,NK,MO,GR,B)]
-
-# write data to disk
 pheno = pheno[keep]
 beta  = beta[,keep]
+
+# Keep only the columns relevant for downstream analysis
+pheno = pheno[,.(gsm,smoker,sex,CD4,CD8,NK,MO,GR,B)]
+
+# Also keep a copy of the manfist
 manifest = copy(meth$manifest)
 
-#save(pheno,manifest,beta,file="data/processed.rda")
+# Write data to disk (do not acutally run the next line)
+# save(pheno,manifest,beta,file="data/processed.rda")
